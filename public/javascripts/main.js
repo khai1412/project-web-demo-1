586398625b51d8140b0a926fbf24a44c3d85b01e
khai1412 <khai1412@gmail.com>
2021-10-10 22:17:39 +0700

    day laf lan commit 1

---
+++ public/javascripts/main.js
@@ -0,0 +1,7 @@
+
+function call_content(){
+    var element1=document.createElement('h1');
+    element1.innerText="that is my project";
+    var element2=document.getElementsByClassName('')[0];
+   element2.appendChild(element1);
+}
